cda41460dcce824d5419db45f161574905e3a5ac
John Sweeney <jes2806@gmail.com>
2022-08-04 15:55:06 -0600

    adding to repo

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,20 @@
 const request = require('request');
-const args = process.argv.splice(2);
-const breed = args[0];
-const breedDomain = 'https://api.thecatapi.com/v1/breeds/search?q=' + breed;
 
-request(breedDomain, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  }
-  const data = JSON.parse(body);
-  if (data[0]) {
-    console.log(data[0].description);
-  } else {
-    console.log('Breed not found');
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const breedDomain = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(breedDomain, (error, _, body) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data[0]) {
+      callback(null, data[0].description);
+    } else {
+      callback('Breed not found');
+    }
+  });
 
-request(breedDomain);
+};
+
+module.exports = { fetchBreedDescription };
